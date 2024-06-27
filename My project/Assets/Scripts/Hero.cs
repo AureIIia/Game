c37404537453097cd983aa095b45d5037872bb34
AureIIia <volkv2118@gmail.com>
2024-06-27 22:20:28 +0300

    Проигрывание анимации
    Проигрывание анимации

--- My project/Assets/Scripts/Hero.cs
+++ My project/Assets/Scripts/Hero.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] private float speed = 4f; // скорость движения
     [SerializeField] private int lives = 5; // количество жизней
     [SerializeField] private float jumpForce = 30f; // сила прыжка  
     [SerializeField] private float airControlFactor = 0.6f; // Контроль в воздухе
-    private bool isGrounded = false;
+    private bool isGrounded = false;    
 
 
     private Rigidbody2D rb;
@@ -16,16 +16,18 @@ public class Hero : MonoBehaviour
     private Animator anim;
     private bool jumpRequest;
 
-
+    
     [SerializeField] private Transform groundCheck; // Точка для проверки, на земле ли персонаж
     [SerializeField] private float checkRadius = 0.2f; // Радиус проверки
     [SerializeField] private LayerMask whatIsGround; // Слой, представляющий землю
+
 
     private States State
     {
         get { return (States)anim.GetInteger("state"); }
         set { anim.SetInteger("state", (int)value); }
     }
+
 
     private void Awake()
     {
@@ -58,10 +60,11 @@ public class Hero : MonoBehaviour
     private void Run()
     {
         if (isGrounded) State = States.run;
+
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-
+    
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
-
+        
         sprite.flipX = dir.x < 0.0f;
     }
 
@@ -78,7 +81,7 @@ public class Hero : MonoBehaviour
             rb.velocity = new Vector2(moveInput * speed * airControlFactor, rb.velocity.y);
         }
     }
-
+   
     private void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
@@ -88,33 +91,15 @@ public class Hero : MonoBehaviour
     {
         Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
         isGrounded = collider.Length > 1;
+
         if (!isGrounded) State = States.jump;
     }
 }
+
 
 public enum States
 {
     idle,
     run,
     jump
-}
-
-public class CameraController : MonoBehaviour
-{
-    [SerializeField] private Transform player;
-    private Vector3 pos;
-
-    private void Awake()
-    {
-        if (!player)
-        {
-           player = FindObjectOfType<Hero>().transform;
-        }
-    }
-    private void Update()
-    {
-        pos = player.position;
-
-        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime);
-    }
 }
