2b510cfb6d33524820113517d6fa942ba6cc039c
likadog <anne.naydenko@gmail.com>
2024-07-14 14:03:15 +0300

    Update Hero.cs

--- My project/Assets/Scripts/Hero.cs
+++ My project/Assets/Scripts/Hero.cs
@@ -103,3 +103,19 @@ public enum States
     run,
     jump
 }
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private Vector3 pos;
+    private void Awake()
+    {
+        if(!player)
+           player = FindObjectOfType<NewBehaviourScript>().transform;
+    }
+    private void Update()
+    {
+        pos = player.position;
+        pos.z = -10f;
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime);
+    }
+}
